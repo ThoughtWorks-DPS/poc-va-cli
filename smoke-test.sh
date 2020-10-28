2980ea0d6691922435df57221e48298b91f29967
richard-guaman <richard.guaman@thoughtworks.com>
2020-10-28 14:27:21 -0400

    [Richard | Nelson] Updated smoke test script and configured circleci to run smoke test

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-curl -L https://github.com/ThoughtWorks-DPS/poc-va-cli/releases/download/v0.1.29/poc-va-cli_0.1.29_Linux_i386.tar.gz --output poc-va-cli_0.1.29_Linux_i386.tar.gz
-tar -xvf poc-va-cli_0.1.29_Linux_i386.tar.gz
-chmod +x poc-va-cli
-./poc-va-cli
+#Should probably change executor if git is going to be used
+CURRENT_VERSION=$(git describe --abbrev=0 | tr -d 'v')
+IMAGE="Linux_i386.tar.gz"
+PROJECT="poc-va-cli"
+
+url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/v${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
+echo $CURRENT_VERSION
+echo $url
+curl -L $url --output ${PROJECT}_${IMAGE}
+tar -xvzf ${PROJECT}_${IMAGE}
+./poc-va-cli get
