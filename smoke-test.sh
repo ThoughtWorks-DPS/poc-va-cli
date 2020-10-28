032447f3fd0720dbe6a6f1e4e3ccfec3be62a0d2
Nelson Liao <nliao@nelsons-mbp.lan>
2020-10-28 12:18:27 -0700

    [Richard/Nelson] Setting current version in smoke test script

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Should probably change executor if git is going to be used
+CURRENT_VERSION=$(git describe --abbrev=0 | tr -d 'v')
 
 url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/v${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
 echo $CURRENT_VERSION
