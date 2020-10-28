ec5a72ca5b1324df650885c29cd83f7707e0e059
Nelson Liao <nliao@nelsons-mbp.lan>
2020-10-28 11:44:50 -0700

    [Richard/Nelson] Using circleci environment variable to direct smoke test

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #Should probably change executor if git is going to be used
-CURRENT_VERSION=$(git describe --abbrev=0 | tr -d 'v')
-IMAGE="Linux_i386.tar.gz"
-PROJECT="poc-va-cli"
 
 url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/v${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
 echo $CURRENT_VERSION
