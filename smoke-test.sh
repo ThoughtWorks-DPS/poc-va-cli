b4b464a5d2b2916b7cbdd832acfea40b0d8bc07a
Nelson Liao <nliao@nelsons-mbp.lan>
2020-10-30 12:10:29 -0700

    [Richard/Nelson] Added windows smoke test to circleci.

--- smoke-test.sh
+++ smoke-test.sh
@@ -5,6 +5,14 @@ CURRENT_VERSION=$(git describe --abbrev=0 | tr -d 'v')
 url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/v${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
 echo $CURRENT_VERSION
 echo $url
+
 curl -L $url --output ${PROJECT}_${IMAGE}
-tar -xvzf ${PROJECT}_${IMAGE}
+
+if [ $IMAGE = "Windows_x86_64.zip" ]; then
+  echo "im in windows"
+  unzip -o ${PROJECT}_${IMAGE}
+else
+  tar -xvzf ${PROJECT}_${IMAGE}
+fi
+
 ./poc-va-cli get
