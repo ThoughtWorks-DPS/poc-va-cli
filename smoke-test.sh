76070db16a846c4223a7c110fe2b3ff0109da3ba
Nelson Liao <nliao@nelsons-mbp.lan>
2020-10-30 12:47:29 -0700

    [Richard/Nelson] Troubleshooting windows smoke test

--- smoke-test.sh
+++ smoke-test.sh
@@ -5,11 +5,10 @@ CURRENT_VERSION=$(git describe --abbrev=0 | tr -d 'v')
 url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/v${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
 
 curl -L $url --output ${PROJECT}_${IMAGE}
-pwd
-echo $IMAGE
+
 if [ $IMAGE = "Windows_x86_64.zip" ]; then
   unzip -o ${PROJECT}_${IMAGE}
-  ./poc-va-cli.exe get
+  ./poc-va-cli.exe get | Write-Output
 else
   tar -xvzf ${PROJECT}_${IMAGE}
   ./poc-va-cli get
