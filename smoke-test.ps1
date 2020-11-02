68583ba11ffcefd396e9a0f5d3cf85959d03ef42
Nelson Liao <nliao@nelsons-mbp.lan>
2020-11-02 13:07:50 -0800

    [Punit/Nelson] Updated powershell syntax with regards to windows smoke test

--- smoke-test.ps1
+++ smoke-test.ps1
@@ -2,7 +2,7 @@ $CURRENT_VERSION = git describe --abbrev=0 | tr -d 'v'
 
 url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/v${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
 
-Invoke-RestMethod -Uri $url -Method Get -ContentType "application/zip"  -OutFile ${PROJECT}_${IMAGE}
-Expand-Archive -LiteralPath ./${PROJECT}_${IMAGE} -DestinationPath ./
+curl -L $url --output ${PROJECT}_${IMAGE}Expand-Archive -LiteralPath ./${PROJECT}_${IMAGE} -DestinationPath ./
+unzip ${PROJECT}_${IMAGE}
 
 ./poc-va-cli get
