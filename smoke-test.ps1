a639d4fefc9dadcb9f55d1ad26da70978139f7ed
Nelson Liao <nliao@nelsons-mbp.lan>
2020-11-02 13:25:48 -0800

    [Nelson] Updated powershell interpolation for windows smoke test

--- smoke-test.ps1
+++ smoke-test.ps1
@@ -1,10 +1,8 @@
 $CURRENT_VERSION = git describe --abbrev=0 | tr -d 'v'
-$IMAGE = "Windows_x86_64.zip"
-$PROJECT = "poc-va-cli"
 
-url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/v${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
+url="https://github.com/ThoughtWorks-DPS/" + $PROJECT + "/releases/download/v" + $CURRENT_VERSION + "/" + $PROJECT + "_" + $CURRENT_VERSION + "_" + $IMAGE
 
-curl -L $url --output ${PROJECT}_${IMAGE}Expand-Archive -LiteralPath ./${PROJECT}_${IMAGE} -DestinationPath ./
-unzip ${PROJECT}_${IMAGE}
+curl -L $url --output $PROJECT + "_" + $IMAGE
+unzip $PROJECT + "_" + $IMAGE
 
 ./poc-va-cli get
