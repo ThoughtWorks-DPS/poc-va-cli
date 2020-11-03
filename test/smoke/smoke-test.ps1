61f23d1f53c940a784297326e4e0c0e760fedabd
Punit Lad <plad@thoughtworks.com>
2020-11-03 11:16:41 -0500

    [Punit] move smoke test script into test folder

---
+++ test/smoke/smoke-test.ps1
@@ -0,0 +1,10 @@
+$CURRENT_VERSION = git describe --abbrev=0 | tr -d 'v'
+$IMAGE = "Windows_x86_64.zip"
+$PROJECT = "poc-va-cli"
+
+$url='https://github.com/ThoughtWorks-DPS/' + $PROJECT + '/releases/download/v' + $CURRENT_VERSION + '/' + $PROJECT + '_' + $CURRENT_VERSION + '_' + $IMAGE
+
+curl -L $url --output cli.zip
+unzip cli.zip -d cli/
+
+./cli/poc-va-cli.exe get
