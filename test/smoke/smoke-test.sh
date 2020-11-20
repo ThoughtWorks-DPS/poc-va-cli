ab8acda0185bcc47c66db4a49c8ec8d6edca4945
Seun Oluwaloju <seunoluwaloju@Seuns-MacBook-Pro.local>
2020-11-20 13:23:33 -0600

    [Seun] - fix smoke test file

--- test/smoke/smoke-test.sh
+++ test/smoke/smoke-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Should probably change executor if git is going to be used
-CURRENT_VERSION=$(git describe --abbrev=0 | tr -d 'v')
+CURRENT_VERSION=$(git describe --abbrev=0)
 
 url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/v${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
 curl -L $url --output ${PROJECT}_${IMAGE}
