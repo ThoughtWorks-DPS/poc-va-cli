19ba7ee051942a30ccd8df15686da14acde50f9a
Seun Oluwaloju <seunoluwaloju@Seuns-MacBook-Pro.local>
2020-11-20 13:34:49 -0600

    [Seun] - fix smoke test

--- test/smoke/smoke-test.ps1
+++ test/smoke/smoke-test.ps1
@@ -2,7 +2,7 @@ $CURRENT_VERSION = git describe --tag --abbrev=0
 $IMAGE = "Windows_x86_64.zip"
 $PROJECT = "poc-va-cli"
 
-$url='https://github.com/ThoughtWorks-DPS/' + $PROJECT + '/releases/download/v' + $CURRENT_VERSION + '/' + $PROJECT + '_' + $CURRENT_VERSION + '_' + $IMAGE
+$url='https://github.com/ThoughtWorks-DPS/' + $PROJECT + '/releases/download/' + $CURRENT_VERSION + '/' + $PROJECT + '_' + $CURRENT_VERSION + '_' + $IMAGE
 
 curl -L $url --output cli.zip
 unzip cli.zip -d cli/
--- test/smoke/smoke-test.sh
+++ test/smoke/smoke-test.sh
@@ -2,7 +2,7 @@
 #Should probably change executor if git is going to be used
 CURRENT_VERSION=$(git describe --abbrev=0)
 
-url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/v${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
+url="https://github.com/ThoughtWorks-DPS/${PROJECT}/releases/download/${CURRENT_VERSION}/${PROJECT}_${CURRENT_VERSION}_${IMAGE}"
 curl -L $url --output ${PROJECT}_${IMAGE}
 tar -xvzf ${PROJECT}_${IMAGE}
 
