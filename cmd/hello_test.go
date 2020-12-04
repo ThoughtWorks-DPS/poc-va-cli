eb0e83bed5029191c867ddef4485c960f1c52676
Nelson Liao <nliao@thoughtworks.com>
2020-12-03 21:38:28 -0800

    Revert "[Nelson] Introduced failing unit test to validate the code climate story"
    This reverts commit 445ca1bdcedd16e0a4cdc7e9a7b6eb79a08e90c1.

--- cmd/hello_test.go
+++ cmd/hello_test.go
@@ -1,11 +1,10 @@
 package cmd
 
 import (
-	"testing"
-	"voltron/clients"
-
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"testing"
+	"voltron/clients"
 )
 
 func TestDoHello(t *testing.T) {
@@ -20,14 +19,3 @@ func TestDoHello(t *testing.T) {
 	assert.Equal(t, "Hello from the API!", doHello(mockedApiClient))
 }
 
-func TestCodeClimate(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockedApiClient := clients.NewMockApiClient(ctrl)
-	mockedApiClient.EXPECT().
-		GetHello().
-		Return("Hello from the API!")
-
-	assert.Equal(t, "Failing test to validate code climate story", doHello(mockedApiClient))
-}
