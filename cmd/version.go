d3bda310f3f2c465cca5f9ef5fa1bfb7a072a153
richardlau <rilau@thoughtworks.com>
2020-12-21 16:51:50 -0500

    [Rich] - Add version command that displays version and git hash

---
+++ cmd/version.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCommand())
+}
+
+func versionCommand() *cobra.Command {
+  return &cobra.Command{
+  	Use: "version",
+  	Short: "Current version of Voltron and API",
+  	Run: func(cmd *cobra.Command, args []string) {
+		printVersion()
+	},
+  }
+}
+
+var Version = "development"
+var Commit = "gitHash"
+
+func printVersion() {
+	fmt.Printf("voltron version: %s, SHA: %s\n", Version, Commit)
+}
