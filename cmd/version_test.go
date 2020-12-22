d5c15537a7f579e7bffcb1752cd5f113b3ad0bca
richardlau <rilau@thoughtworks.com>
2020-12-22 15:06:09 -0500

    [Rich] - Add integration test for version cmd

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -1,11 +1,24 @@
 package cmd
 
+import (
+	"github.com/kami-zh/go-capturer"
+	"strings"
+	"testing"
+)
+
 func ExampleTestVersionWithDefaults() {
 	printVersion()
 	//Output:
 	//voltron version: development, SHA: gitHash
 }
 
-func ExampleTestVersionWithOverride() {
+func TestVersionCmdIntegration(t *testing.T) {
+	cmd := versionCmd()
+	out := capturer.CaptureStdout(func() {
+		cmd.ExecuteC()
+	})
 
+	if !strings.Contains(string(out), "voltron version: development, SHA: gitHash") {
+		t.Fatalf("expected \"%s\" got \"%s\"", "voltron version: development, SHA: gitHash", string(out))
+	}
 }
