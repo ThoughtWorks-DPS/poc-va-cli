fd0ed9163eb0ee005eb31be84e1854a7fca90aa3
richardlau <rilau@thoughtworks.com>
2020-12-23 17:07:17 -0500

    [Rich] - Update cc threshold check to be more detailed

--- ccThresholdCheck.sh
+++ ccThresholdCheck.sh
@@ -7,7 +7,7 @@ THRESHOLD=90.0
 if test -f "$FILE"; then
     COVERAGE=$(go tool cover -func cover.out | grep total | awk '{print substr($3, 1, length($3)-1)}')
     if [[ ( $COVERAGE < $THRESHOLD ) ]]; then
-      echo "Code coverage is below the threshold. Failing pipeline..."
+      echo "The code coverage of $COVERAGE% is below the $THRESHOLD% threshold. Failing pipeline..."
       exit 1
     fi
 else
