5ad27991cdc461a11999d1fa177623b1ce6d5a47
richardlau <rilau@thoughtworks.com>
2020-12-23 17:12:04 -0500

    [Rich] - Fix broken build

--- ccThresholdCheck.sh
+++ ccThresholdCheck.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-FILE=./cover.out
+FILE=./c.out
 THRESHOLD=80.0
 
 if test -f "$FILE"; then
-    COVERAGE=$(go tool cover -func cover.out | grep total | awk '{print substr($3, 1, length($3)-1)}')
+    COVERAGE=$(go tool cover -func c.out | grep total | awk '{print substr($3, 1, length($3)-1)}')
     if [[ ( $COVERAGE < $THRESHOLD ) ]]; then
       echo "The code coverage of $COVERAGE% is below the $THRESHOLD% threshold. Failing pipeline..."
       exit 1
